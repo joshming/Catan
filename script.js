4889782775ed8351da9216e03ec79203a230fb9a
josh <josh.jm2011@gmail.com>
2020-05-12 19:05:56 -0400

    git

---
+++ script.js
@@ -0,0 +1,13 @@
+function showjoinform(){
+    var container = document.getElementById('container')
+    container.classList.remove('right-active');
+}
+
+function shownewgame(){
+    var container = document.getElementById('container')
+    container.classList.add('right-active')
+}
+
+function completeAndRedirect(){
+    
+}
